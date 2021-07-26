c2eee9ef304b95e56dd6e7d6f206e15603a3561c
Tristan Kuehn <tristankuehn@gmail.com>
2021-07-26 10:13:33 -0400

    Fix pylint warnings

--- graham-bidsbatch.py
+++ graham-bidsbatch.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+"""Submit a job to Graham, translating sshfs mounts."""
+
 import argparse
 import subprocess
+from pathlib import Path
 
 all_mounts = str(
     subprocess.run(
@@ -13,7 +16,9 @@ all_mounts = str(
 ).splitlines()
 
 sshfs_mounts = [
-    {mount.split()[0]: mount.split()[1]} for mount in all_mounts if "sshfs" in mount
+    {Path(mount.split()[0]): mount.split()[1]}
+    for mount in all_mounts
+    if "sshfs" in mount
 ]
 
 parser = argparse.ArgumentParser()
@@ -24,6 +29,14 @@ parser.add_argument("out_dir")
 parser.add_argument("analysis_level")
 
 args = parser.parse_args()
+
+# check if bids_dir and out_dir are children of an active sshfs mount
+bids_dir = Path(args.bids_dir).resolve(strict=True)
+out_dir = Path(args.out_dir).resolve(strict=True)
+
+for local_path in sshfs_mounts.keys():
+    if local_path.parts == bids_dir.parts[:len(local_path.parts)]:
+        print("Parent found")
 
 subprocess.run(
     [
